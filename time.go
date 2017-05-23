74532d5065a840450832c19cac8af3bbefab864c
Peter Hellberg <peter@c7.se>
2017-05-23 12:31:05 +0200

    Update UnmarshalXMLAttr to handle multiple formats
    Supported formats:
    
    	- time.RFC3339
        - 2006-01-02T15:04:05
        - 2006-01-02
    
    If missing time zone then Europe/Stockholm is used
    
    Issue #1

--- time.go
+++ time.go
@@ -20,7 +20,34 @@ type Time struct {
 }
 
 func (t *Time) UnmarshalXMLAttr(attr xml.Attr) error {
-	pt, err := time.ParseInLocation("2006-01-02T15:04:05", attr.Value, Stockholm)
+	switch attr.Value {
+	case "0001-01-01T00:00:00+01:00":
+		attr.Value = "0001-01-01T00:00:00+00:00"
+	case "9999-12-31T23:59:59+01:00":
+		attr.Value = "9999-12-31T23:59:59+00:00"
+	}
+
+	var format string
+
+	switch len(attr.Value) {
+	case 25:
+		pt, err := time.Parse(time.RFC3339, attr.Value)
+		if err != nil {
+			return err
+		}
+
+		*t = Time{pt}
+
+		return nil
+	case 19:
+		format = "2006-01-02T15:04:05"
+	case 10:
+		format = "2006-01-02"
+	case 0:
+		return nil
+	}
+
+	pt, err := time.ParseInLocation(format, attr.Value, Stockholm)
 	if err != nil {
 		return err
 	}
--- time_test.go
+++ time_test.go
@@ -4,9 +4,44 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"encoding/xml"
+	"errors"
 	"testing"
 	"time"
 )
+
+func TestUnmarshalXMLAttr(t *testing.T) {
+	for _, tt := range []struct {
+		attr xml.Attr
+		want time.Time
+		err  error
+	}{
+		{xml.Attr{}, time.Time{}, nil},
+		{xml.Attr{Value: "2017-01-02"}, time.Date(2017, 1, 2, 0, 0, 0, 0, Stockholm), nil},
+		{xml.Attr{Value: "2017-01-02T14:28:56"}, time.Date(2017, 1, 2, 14, 28, 56, 0, Stockholm), nil},
+		{xml.Attr{Value: "2017-01-02T14:28:56+02:00"}, time.Date(2017, 1, 2, 13, 28, 56, 0, Stockholm), nil},
+		{xml.Attr{Value: "not-a-date"}, time.Time{}, errors.New(
+			`parsing time "not-a-date" as "2006-01-02": cannot parse "not-a-date" as "2006"`,
+		)},
+	} {
+		et := &Time{}
+
+		err := et.UnmarshalXMLAttr(tt.attr)
+		if err != nil {
+			if tt.err == nil {
+				t.Fatalf("et.UnmarshalXMLAttr(%#v) = %v, want nil", tt.attr, err)
+			}
+
+			if got, want := err.Error(), tt.err.Error(); got != want {
+				t.Fatalf("err.Error() = %v, want %v", got, want)
+			}
+		}
+
+		if !et.Time.Equal(tt.want) {
+			t.Fatalf("%v != %v", et.Time, tt.want)
+		}
+	}
+}
 
 func TestMarshalJSON(t *testing.T) {
 	t.Run("nil", func(t *testing.T) {
